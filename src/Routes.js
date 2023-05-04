777dcd3ed34e2329e0c4678fb0dd18d852a8b436
Christhofer <christhoferxx@gmail.com>
2023-05-04 00:12:21 -0300

    configurando novas rotas e pagina de login

--- src/Routes.js
+++ src/Routes.js
@@ -3,10 +3,16 @@ import { useRoutes } from 'react-router-dom';
 
 import Home from './pages/Home';
 import About from './pages/About';
+import NotFound  from "./pages/NotFound";
+import SignIn from './pages/SignIn';
+
+
 
 export const Routes = () => {
     return useRoutes([
         { path: "/", element: <Home/>},
-        { path: "/about", element: <About/>}
+        { path: "/about", element: <About/>},
+        { path: "/signin", element: <SignIn/>},
+        { path:'*', element: <NotFound/>},
     ])
 }
