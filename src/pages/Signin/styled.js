6005ff5c6a1376ac2f6a5f11806f981dad8944fd
Christhofer <christhoferxx@gmail.com>
2023-05-04 00:11:50 -0300

    configurando novas rotas e iniciando a pagina de login

---
+++ src/pages/Signin/styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const PageArea = styled.div`
+form {
+    background-color: #FFF;
+    border-radius: 3px;
+    padding: 10px;
+    box-shadow: 0px 0px 3px #999;
+
+    .area {
+        
+    }
+}
+`;
